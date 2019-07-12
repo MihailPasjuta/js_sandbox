60efa4dbe9af87070839d00c15db13bafcd906d9
mihail <mihailpaslevel@gmail.com>
2019-07-12 22:05:41 +0300

    Commit

---
+++ module_2/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Registration from './registration';
+	ReactDOM.render((
+	<Registration></Registration>
+	), document.getElementById('root'));
